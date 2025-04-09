31be1303bdbf78768d96087e295f5fd9b3547040
Nathaniel Smith <qwrx123@gmail.com>
2025-04-09 10:41:38 -0700

    Initial Commit

---
+++ QTExample.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Basic Qt6 Application");
+
+    window.show();
+
+    return app.exec();
+}
