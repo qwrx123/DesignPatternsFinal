bb66b3864ab77df69b73efa7710db3f4cf76ec1f
Sharderai <shadowstardiamond@yahoo.com>
2025-04-30 13:38:18 -0700

    Updated CMakeLists to allow ButtonClass to compile, and made buttons look a bit more button-like

--- classes/ButtonClass.cpp
+++ classes/ButtonClass.cpp
@@ -1,45 +1,54 @@
-#include <GLFW/glfw3.h>
 #include "ButtonClass.h"
+#include <GLFW/glfw3.h>
 
-ButtonClass::ButtonClass(double x, double y, double width, double height, double percentRed, double percentGreen, double percentBlue)
-    : left(x), top(y), red(percentRed), green(percentGreen), blue(percentBlue)
+ButtonClass::ButtonClass(double x, double y, double width, double height, double percentRed,
+						 double percentGreen, double percentBlue)
+	: left(x), top(y), red(percentRed), green(percentGreen), blue(percentBlue)
 {
-    right = left + width - 1;
-    bottom = top + height - 1;
+	right  = left + width - 1;
+	bottom = top + height - 1;
 }
 
-ButtonClass::~ButtonClass()
-{
-
-}
+ButtonClass::~ButtonClass() {}
 
 double ButtonClass::getTop()
 {
-    return top;
+	return top;
 }
 
 double ButtonClass::getBottom()
 {
-    return bottom;
+	return bottom;
 }
 
 double ButtonClass::getLeft()
 {
-    return left;
+	return left;
 }
 
 double ButtonClass::getRight()
 {
-    return right;
+	return right;
 }
 
 void ButtonClass::renderButton()
 {
-    glBegin(GL_QUADS);
-        glColor3f(red, green, blue);
-        glVertex2f(left, top);
-        glVertex2f(right, top);
-        glVertex2f(right, bottom);
-        glVertex2f(left, bottom);
-    glEnd();
+	glBegin(GL_QUADS);
+	glColor3f(.8, .8, .8);
+	glVertex2f(left, top);
+	glColor3f(.7, .7, .7);
+	glVertex2f(right, top);
+	glColor3f(.3, .3, .3);
+	glVertex2f(right, bottom);
+	glColor3f(.5, .5, .5);
+	glVertex2f(left, bottom);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3f(red, green, blue);
+	glVertex2f(left + 3, top + 3);
+	glVertex2f(right - 3, top + 3);
+	glVertex2f(right - 3, bottom - 3);
+	glVertex2f(left + 3, bottom - 3);
+	glEnd();
 }
