0926ca006fc3b3f4d34c8cd6ad4f791eb24c1dbe
JordenWB <kittyjeanette1@gmail.com>
2025-05-01 10:56:48 -0700

    what I forgot

--- include/WindowClass.h
+++ include/WindowClass.h
@@ -1,5 +1,6 @@
 #include <GLFW/glfw3.h>
 #include <vector>
+#include "TextClass.h"
 
 class WindowClass
 {
@@ -33,6 +34,7 @@ class WindowClass
    protected:
 	static void characterCallback(GLFWwindow* window, unsigned int codepoint);
 	void		handleCharacterInput(unsigned int codepoint);
+	TextClass*	m_textHandler{nullptr};
 
 	// Tools
    protected:
