ab11e5945ef9212ed205530c5d07d4ebc7adb031
JordenWB <kittyjeanette1@gmail.com>
2025-05-01 08:55:28 -0700

    got Freetype in

--- include/TextClass.h
+++ include/TextClass.h
@@ -1,11 +1,16 @@
 #include <string>
-
+#include <ft2build.h> // FreeType2 header files
+#include FT_FREETYPE_H // FreeType2 header files
 // Use a .ttf font file for text rendering
 class TextClass
 {
     public:
      TextClass() = default;
      ~TextClass() = default;
+     FT_Library library;
+     FT_Face face;
+     error = FT_Init_FreeType(&library);
+     error = FT_New_Face(library, "path/to/font.ttf", 0, &face);
 
     protected:
         std::string text;
