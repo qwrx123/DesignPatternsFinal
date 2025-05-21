4be8515b77e86ca1b76a3208457def2d459a6337
Sharderai <shadowstardiamond@yahoo.com>
2025-05-20 23:08:11 -0700

    updated ButtonClass to have IButton interface methods

--- classes/ButtonClass.cpp
+++ classes/ButtonClass.cpp
@@ -1,5 +1,8 @@
 #include "ButtonClass.h"
 #include <GLFW/glfw3.h>
+#include <functional>
+#include <string>
+#include "Bounds.h"
 
 ButtonClass::ButtonClass(double x, double y, double width, double height, double percentRed,
 						 double percentGreen, double percentBlue)
@@ -10,6 +13,49 @@ ButtonClass::ButtonClass(double x, double y, double width, double height, double
 }
 
 ButtonClass::~ButtonClass() {}
+
+void ButtonClass::setBounds(const Bounds& bounds)
+{
+	this->bounds = bounds;
+}
+
+Bounds ButtonClass::getBounds() const
+{
+	return bounds;
+}
+
+bool ButtonClass::isHovered() const
+{
+	return hovered;
+}
+
+void ButtonClass::setHovered(bool hovered)
+{
+	this->hovered = hovered;
+}
+
+bool ButtonClass::isPressed() const
+{
+	return pressed;
+}
+
+void ButtonClass::setPressed(bool pressed)
+{
+	this->pressed = pressed;
+}
+
+std::string ButtonClass::getLabel() const
+{
+	return label;
+}
+
+void ButtonClass::setLabel(const std::string& label)
+{
+	this->label = label;
+}
+
+// customize for specialized button classes inheriting from ButtonClass
+void ButtonClass::onClick(std::function<void()> callback) {}
 
 double ButtonClass::getTop()
 {
--- include/ButtonClass.h
+++ include/ButtonClass.h
@@ -2,13 +2,31 @@
 #define BUTTON_CLASS_H
 
 #include <GLFW/glfw3.h>
+#include <functional>
+#include <string>
+#include "Bounds.h"
+#include "IButton.h"
 
-class ButtonClass
+class ButtonClass : public IButton
 {
    public:
 	ButtonClass(double x, double y, double width, double height, double percentRed,
 				double percentGreen, double percentBlue);
 	~ButtonClass();
+
+	void   setBounds(const Bounds& bounds) override;
+	Bounds getBounds() const override;
+
+	bool isHovered() const override;
+	void setHovered(bool hovered) override;
+	bool isPressed() const override;
+	void setPressed(bool pressed) override;
+
+	std::string getLabel() const override;
+	void		setLabel(const std::string& label) override;
+
+	void onClick(std::function<void()> callback) override;
+
 	double getTop();
 	double getBottom();
 	double getLeft();
@@ -16,6 +34,11 @@ class ButtonClass
 	void   renderButton();
 
    private:
+	Bounds		bounds;
+	bool		hovered;
+	bool		pressed;
+	std::string label;
+
 	double top;
 	double bottom;
 	double left;
