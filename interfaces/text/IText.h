79b3a33fd96b18e00bc33d6c1d9fc627b2bdc60e
Nathaniel Smith <qwrx123@gmail.com>
2025-05-23 09:38:01 -0700

    Minimal warnings in IText.h

--- interfaces/text/IText.h
+++ interfaces/text/IText.h
@@ -10,23 +10,23 @@ class IText
    public:
 	virtual ~IText() = default;
 
-	virtual std::string getContent() const					= 0;
-	virtual void		setContent(const std::string& text) = 0;
+	[[nodiscard]] virtual std::string getContent() const				  = 0;
+	virtual void					  setContent(const std::string& text) = 0;
 
-	virtual Bounds getBounds() const			   = 0;
-	virtual void   setPosition(double x, double y) = 0;
+	[[nodiscard]] virtual Bounds getBounds() const				 = 0;
+	virtual void				 setPosition(double x, double y) = 0;
 
-	virtual std::string getFontName() const						 = 0;
-	virtual void		setFontName(const std::string& fontName) = 0;
+	[[nodiscard]] virtual std::string getFontName() const					   = 0;
+	virtual void					  setFontName(const std::string& fontName) = 0;
 
-	virtual int	 getFontSize() const   = 0;
-	virtual void setFontSize(int size) = 0;
+	[[nodiscard]] virtual int getFontSize() const	= 0;
+	virtual void			  setFontSize(int size) = 0;
 
-	virtual Color getColor() const			   = 0;
-	virtual void  setColor(const Color& color) = 0;
+	[[nodiscard]] virtual Color getColor() const			 = 0;
+	virtual void				setColor(const Color& color) = 0;
 
-	virtual bool isEditable() const			= 0;
-	virtual void setEditable(bool editable) = 0;
+	[[nodiscard]] virtual bool isEditable() const		  = 0;
+	virtual void			   setEditable(bool editable) = 0;
 };
 
 #endif	// ITEXT_H
