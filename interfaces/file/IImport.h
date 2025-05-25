310522ac714c9fbdb4552703b8ca7db2545b6308
Nathaniel Smith <qwrx123@gmail.com>
2025-05-24 18:43:18 -0700

    No warnings in  IFiles.h

--- interfaces/file/IImport.h
+++ interfaces/file/IImport.h
@@ -5,7 +5,12 @@
 class IImport : IFiles
 {
    public:
-	virtual ~IImport() = default;
+	IImport()						   = default;
+	virtual ~IImport()				   = default;
+	IImport(const IImport&)			   = default;
+	IImport& operator=(const IImport&) = default;
+	IImport(IImport&&)				   = default;
+	IImport& operator=(IImport&&)	   = default;
 
 	[[nodiscard]] virtual void importFile(void*, ssize_t) = 0;
 };
