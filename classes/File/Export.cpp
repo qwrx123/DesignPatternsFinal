faf43cdef06b842d61f3e339bf14104cc932b671
Nathaniel Smith <qwrx123@gmail.com>
2025-06-02 10:51:16 -0700

    Test failed

--- classes/File/Export.cpp
+++ classes/File/Export.cpp
@@ -1,9 +1,11 @@
 #include "Export.h"
 #include "FileLocation.h"
 
-void exportFile(void* fileLocation, ssize_t fileSize) {}
+void Export::exportFile(void* fileLocation, ssize_t fileSize) {}
 
-std::string quarryFileLocation()
+std::string Export::quarryFileLocation()
 {
 	return FileLocation::getDownloadLocation();
 }
+
+void Export::setFileType(IFiles::type fileType) {}
--- test/FileTests.cpp
+++ test/FileTests.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
-
+#include <filesystem>
 #include "Export.h"
 
-TEST(FileTests, FileExists) {
+TEST(FileTests, FolderExists) {
+    Export exportFile = Export();
+    std::string location = exportFile.quarryFileLocation();
     
+    std::filesystem::path path(location);
+    EXPECT_TRUE(std::filesystem::exists(path));
 }
