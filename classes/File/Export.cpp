59d2e297cfc62f8d6c8f022a878fd81d8fcb447f
Nathaniel Smith <qwrx123@gmail.com>
2025-06-05 11:58:34 -0700

    BITMAPV5HEADER can be created

--- classes/File/Export.cpp
+++ classes/File/Export.cpp
@@ -77,6 +77,7 @@ bool Export::exportBmpFile(bufferStruct pixels, imageInfo imageInfo)
 	char* buffer = bmpBuffer.data();
 
 	setupBmpFileHeader(buffer, bmpBuffer.size(), imageInfo);
+	setupBmpV5Header(buffer, bmpBuffer.size(), imageInfo);
 
 	return false;
 }
@@ -145,5 +146,45 @@ bool Export::setupBmpV5Header(char* buffer, size_t buffer_size, imageInfo imageI
 
 	setupBmpV1Header(buffer, buffer_size, imageInfo);
 
-	return false;
+	const WORD PIXEL_SIZE  = 32;
+	const LONG COMPRESSION = 3;
+
+	const WORD ALIGNMENT = 4;
+	const WORD PADDING	 = ALIGNMENT - (((imageInfo.width * PIXEL_SIZE) / CHAR_BIT) % ALIGNMENT);
+
+	const DWORD RED_MASK   = 0xFF000000;
+	const DWORD GREEN_MASK = 0x00FF0000;
+	const DWORD BLUE_MASK  = 0x0000FF00;
+	const DWORD ALPHA_MASK = 0x000000FF;
+
+	const DWORD LCS_sRGB = 0x73524742; /* 'sRGB' */
+
+	// Move the buffer pointer to the start of the BITMAPINFOHEADER
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+	buffer += sizeof(BITMAPFILEHEADER);
+	// Only way to make the struct correct
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+	auto* infoHeader		   = reinterpret_cast<BITMAPV5HEADER*>(buffer);
+	infoHeader->bV5Size		   = sizeof(BITMAPV5HEADER);
+	infoHeader->bV5BitCount	   = static_cast<WORD>(PIXEL_SIZE);
+	infoHeader->bV5Compression = COMPRESSION;
+	infoHeader->bV5SizeImage =
+		imageInfo.height * ((imageInfo.width * (PIXEL_SIZE / CHAR_BIT)) + PADDING);
+	infoHeader->bV5RedMask	   = RED_MASK;
+	infoHeader->bV5GreenMask   = GREEN_MASK;
+	infoHeader->bV5BlueMask	   = BLUE_MASK;
+	infoHeader->bV5AlphaMask   = ALPHA_MASK;
+	infoHeader->bV5CSType	   = LCS_sRGB;
+	infoHeader->bV5Endpoints   = {.ciexyzRed   = {.ciexyzX = 0, .ciexyzY = 0, .ciexyzZ = 0},
+								  .ciexyzGreen = {.ciexyzX = 0, .ciexyzY = 0, .ciexyzZ = 0},
+								  .ciexyzBlue  = {.ciexyzX = 0, .ciexyzY = 0, .ciexyzZ = 0}};
+	infoHeader->bV5GammaRed	   = 0;
+	infoHeader->bV5GammaGreen  = 0;
+	infoHeader->bV5GammaBlue   = 0;
+	infoHeader->bV5Intent	   = 0;
+	infoHeader->bV5ProfileData = 0;
+	infoHeader->bV5ProfileSize = 0;
+	infoHeader->bV5Reserved	   = 0;
+
+	return true;
 }
