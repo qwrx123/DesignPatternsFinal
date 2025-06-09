cc9717db6b2da082139844d451758002eff12ae6
Nathaniel Smith <qwrx123@gmail.com>
2025-06-09 16:01:55 -0700

    Test passes

--- classes/File/Import.cpp
+++ classes/File/Import.cpp
@@ -3,15 +3,15 @@
 
 bool Import::importFile()
 {
-    switch (fileType)
-    {
-        case IFiles::type::txt:
-            return readTxtFile();
-        case IFiles::type::bmp:
-            return false;
-        default:
-            return false;
-    }
+	switch (fileType)
+	{
+		case IFiles::type::txt:
+			return readTxtFile();
+		case IFiles::type::bmp:
+			return false;
+		default:
+			return false;
+	}
 }
 
 std::pair<bufferStruct, imageInfo> Import::getImportedData()
@@ -26,39 +26,64 @@ std::string Import::quarryFileLocation()
 
 void Import::setFileLocation(const std::string& fileLocation)
 {
-    this->fileLocation = fileLocation;
+	this->fileLocation = fileLocation;
 }
 
 void Import::setFileType(IFiles::type fileType)
 {
-    this->fileType = fileType;
+	this->fileType = fileType;
 }
 
 void Import::setFileName(const std::string& fileName)
 {
-    this->fileName = fileName;
+	this->fileName = fileName;
 }
 
 bool Import::setBuffer(bufferStruct buffer)
 {
-    importedBuffer = std::move(buffer);
-    return true;
+	importedBuffer = std::move(buffer);
+	return true;
 }
 
 bool Import::setImageInfo(imageInfo info)
 {
 	importedImageInfo = info;
-    return true;
+	return true;
 }
 
 bool Import::readTxtFile()
 {
-    std::string							  fullpath = fileLocation + fileName + ".txt";
+	std::string							  fullpath = fileLocation + fileName + ".txt";
 	std::unique_ptr<FILE, int (*)(FILE*)> file(std::fopen(fullpath.c_str(), "rt"), &fclose);
 	if (!file)
 	{
 		return false;
 	}
 
-    
+	if (std::fseek(file.get(), 0, SEEK_END) != 0)
+	{
+		return false;
+	}
+
+	long fileSize = std::ftell(file.get());
+	if (fileSize == -1)
+	{
+		return false;
+	}
+
+	if (std::fseek(file.get(), 0, SEEK_SET) != 0)
+	{
+		return false;
+	}
+
+	importedBuffer.bufferSize = static_cast<size_t>(fileSize);
+	// When working with files and raw memory, it is neccisary touse char[]
+	// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+	importedBuffer.bufferLocation = std::make_unique<char[]>(importedBuffer.bufferSize);
+
+	// Read file content
+	size_t readBytes = std::fread(importedBuffer.bufferLocation.get(), sizeof(char),
+								  importedBuffer.bufferSize, file.get());
+
+	return readBytes == importedBuffer.bufferSize;
 }
